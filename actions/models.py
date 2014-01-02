8565f37f8b5f7c5adb5ff5784cf90d5c8ba7026e
Eric Neuman <soloptimus@gmail.com>
2014-01-02 02:14:59 -0500

    Moved Actions into it's own app for reproducibility. Probably gonna make that it's own repo.

---
+++ actions/models.py
@@ -0,0 +1,41 @@
+from django.core.urlresolvers import reverse
+
+class Action(object):
+    '''
+    Object to hold and serialize the in-memory relationship between urls and objects and users.
+    '''
+    url = "Unspecified"
+    display_name = "Unspecified"
+
+    def __init__(self, target):
+        self.target = target
+
+    def get_url(self):
+        return self.url
+
+    def get_display_name(self):
+        return self.display_name
+
+    def is_available(self, person):
+        "takes a user and always returns True or False"
+        return True
+
+    def get_serialized(self):
+        return {
+            "url":self.get_url(),
+            "display_name":self.get_display_name()
+        }
+
+class Actionable(object):
+    '''
+    Mixin intended to operate with Action, most likely in a django Model.
+    '''
+    def get_actions(self):
+        return []
+
+    def get_available_actions(self, user):
+        output = []
+        for a in self.get_actions():
+            if a.is_available(person=self):
+                output.append(a.get_serialized())
+        return output
--- core/models.py
+++ core/models.py
@@ -4,46 +4,9 @@ from djmoney.models.fields import MoneyField
 from django.contrib.auth.models import User
 from django.db.models import Sum
 from django.core.urlresolvers import reverse
+from actions.models import Action, Actionable
 
-class Action(object):
-    '''
-    Object to hold and serialize the in-memory relationship between urls and objects and users.
-    '''
-    url = "Unspecified"
-    display_name = "Unspecified"
 
-    def __init__(self, target):
-        self.target = target
-
-    def get_url(self):
-        return self.url
-
-    def get_display_name(self):
-        return self.display_name
-
-    def is_available(self, person):
-        "takes a user and always returns True or False"
-        return True
-
-    def get_serialized(self):
-        return {
-            "url":self.get_url(),
-            "display_name":self.get_display_name()
-        }
-
-class Actionable(object):
-    '''
-    Mixin intended to operate with Action, most likely in a django Model.
-    '''
-    def get_actions(self):
-        return []
-
-    def get_available_actions(self, user):
-        output = []
-        for a in self.get_actions():
-            if a.is_available(person=self):
-                output.append(a.get_serialized())
-        return output
 
 MEDIUM_CHOICES = (
     ('TXT', 'Text'),
