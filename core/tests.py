3e82ddc3cf342cf8c8802ca84c5eb94584adad37
Eric Neuman <soloptimus@gmail.com>
2014-01-16 01:24:39 -0500

    all tests passing once more

--- core/tests.py
+++ core/tests.py
@@ -15,11 +15,17 @@ class ProjectTestCase(TestCase):
             u.delete()
 
     def test_history(self):
-        """history models are working"""
+        """see if history models are working"""
         self.projects[0].title = 'Beta Project'
+        self.projects[0].changed_by = self.users[0]
         self.projects[0].save()
-        history = self.users[0].history.all() 
+        history = self.projects[0].history.all() 
         self.assertNotEqual(history.count(), 2)
+
+    def test_touches(self):
+        """see if touches are rendering correctly"""
+        touch = self.projects[0].get_touches()[0]
+        self.assertEqual(touch['user'].id, self.users[0].id)
 
 
 def build_user():
@@ -28,7 +34,7 @@ def build_user():
     return user
 
 def build_project(members):
-    project = cm.Project(title='Alpha Project', brief='here is some text.', medium='TXT', ask=3000)
+    project = cm.Project(title='Alpha Project', brief='here is some text.', medium='TXT', ask=3000, changed_by=members[0])
     project.save()
     for m in members:
         project.members.add(m)
