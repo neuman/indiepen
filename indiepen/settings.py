b79f73f3bd42406ab70c31eccf5ed726cf190dd6
Eric Neuman <soloptimus@gmail.com>
2014-03-26 18:22:26 -0400

    updated settings to make redis the result backend

--- indiepen/settings.py
+++ indiepen/settings.py
@@ -178,3 +178,5 @@ LOGIN_REDIRECT_URL = '/'
 
 #celery / redis
 BROKER_URL = 'redis://localhost:6379/0'
+BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}  # 1 hour.
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
